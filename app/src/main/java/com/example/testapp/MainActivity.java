638dd95feba6db994961dd2596f7beb308f4f6c2
Jamp666 <rez087@mail.ru>
2020-04-15 11:15:28 +0900

    Api ded

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -19,9 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         private String Location;
         private String API = "e90a73a8a229cb20d0e51447fb2b3b7a";
         private TextView temperature, address, lastUpdate;
-        private Button buttonAldan;
-        private Button buttonYakutsk, buttonAbiy, buttonOymakon ;
-        private ProgressBar progressBar;
+        private ProgressBar loadingCircle;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +30,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         private void initButtons() {
-                buttonYakutsk = findViewById(R.id.yakutsk);
-                buttonYakutsk.setBackgroundColor(Color.TRANSPARENT);
-                buttonAbiy = findViewById(R.id.abiy);
-                buttonAbiy.setBackgroundColor(Color.TRANSPARENT);
-                buttonOymakon = findViewById(R.id.oymyakon);
-                buttonOymakon.setBackgroundColor(Color.TRANSPARENT);
-                buttonAldan = findViewById(R.id.aldan_button);
-//                buttonAldan.setBackgroundColor(Color.TRANSPARENT);
-                Log.e("ALDAN", String.valueOf(buttonAldan == null));
-                progressBar = findViewById(R.id.progressBar);
-                progressBar.getIndeterminateDrawable().setColorFilter(0xFF782D1E, PorterDuff.Mode.MULTIPLY);
-                progressBar.setVisibility(View.INVISIBLE);
+                loadingCircle = findViewById(R.id.progressBar);
+                loadingCircle.setVisibility(View.INVISIBLE);
+
                 List<Button> buttons = new LinkedList<>();
-                buttons.add(buttonAbiy);
-                buttons.add(buttonOymakon);
-                buttons.add(buttonYakutsk);
-//                buttons.add(buttonAldan);
+                buttons.add((Button) findViewById(R.id.abiy));
+                buttons.add((Button) findViewById(R.id.oymyakon));
+                buttons.add((Button) findViewById(R.id.yakutsk));
+                buttons.add((Button) findViewById(R.id.aldan));
+                buttons.add((Button) findViewById(R.id.allayiha));
+                buttons.add((Button) findViewById(R.id.anabar));
+                buttons.add((Button) findViewById(R.id.bulun));
+                buttons.add((Button) findViewById(R.id.uoheHalima));
+                buttons.add((Button) findViewById(R.id.uoheJaani));
+                buttons.add((Button) findViewById(R.id.buluu));
+                buttons.add((Button) findViewById(R.id.uoheeBuluu));
+                buttons.add((Button) findViewById(R.id.ejigeen));
+                buttons.add((Button) findViewById(R.id.kebeei));
+                buttons.add((Button) findViewById(R.id.lenskei));
+                buttons.add((Button) findViewById(R.id.meneKanalas));
+                buttons.add((Button) findViewById(R.id.mirney));
+                buttons.add((Button) findViewById(R.id.muoma));
                 for (Button b : buttons) {
                         b.setOnClickListener(this);
+                        b.setBackgroundColor(Color.TRANSPARENT);
                 }
         }
 
@@ -68,12 +69,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         case R.id.yakutsk :
                                 Location = "yakutsk";
                                 break;
-                        case R.id.aldan_button:
+                        case R.id.aldan:
                                 Location = "aldan";
+                                break;
+                        case R.id.allayiha :
+                                Location = "chokurdakh";
+                                break;
+                        case R.id.anabar :
+                                Location = "saskylakh";
+                                break;
+                        case R.id.bulun :
+                                Location = "tiksi";
+                                break;
+                        case R.id.uoheHalima :
+                                Location = "zyryanka";
+                                break;
+                        case R.id.uoheJaani:
+                                Location = "batagay";
+                                break;
+                        case R.id.buluu :
+                                Location = "vilyuysk";
+                                break;
+                        case R.id.uoheeBuluu :
+                                Location = "verkhnevilyuysk";
+                                break;
+                        case R.id.ejigeen :
+                                Location = "zhigansk";
+                                break;
+                        case R.id.kebeei :
+                                Location = "sangar";
+                                break;
+                        case R.id.lenskei :
+                                Location = "lensk";
+                                break;
+                        case R.id.meneKanalas :
+                                Location = "nizhniy bestyakh";
+                                break;
+                        case R.id.mirney :
+                                Location = "mirnyy";
+                                break;
+                        case R.id.muoma :
+                                Location = "khonuu";//Api no workee
                                 break;
                 }
                 new WeatherTask(this).execute();
-                progressBar.setVisibility(View.VISIBLE);
+                temperature.setVisibility(View.INVISIBLE);
+                address.setVisibility(View.INVISIBLE);
+                lastUpdate.setVisibility(View.INVISIBLE);
+                loadingCircle.setVisibility(View.VISIBLE);
         }
 
         public String getLocation() {
@@ -97,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         public ProgressBar getProgressBar() {
-                return progressBar;
+                return loadingCircle;
         }
 }
 
--- app/src/main/java/com/example/testapp/WeatherTask.java
+++ app/src/main/java/com/example/testapp/WeatherTask.java
@@ -3,8 +3,6 @@ package com.example.testapp;
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.androdocs.httprequest.HttpRequest;
 
@@ -17,30 +15,20 @@ import java.util.Locale;
 
 @SuppressLint("StaticFieldLeak")
 public class WeatherTask extends AsyncTask<String, Void, String> {
-        private MainActivity main;
-        private TextView temperature, addresss, lastUpdate;
-        private ProgressBar progressBar;
+        private MainActivity mainActivity;
 
-
-        public WeatherTask(MainActivity main) {
-                this.main = main;
-                temperature = main.getTemperature();
-                addresss = main.getAddress();
-                lastUpdate = main.getLastUpdate();
-                progressBar = main.getProgressBar();
+        public WeatherTask(MainActivity mainActivity) {
+                this.mainActivity = mainActivity;
         }
 
         @Override
         protected String doInBackground(String... strings) {
-                return HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + main.getLocation() + "&units=metric&appid=" + main.getAPI());
+                return HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + mainActivity.getLocation() + "&units=metric&appid=" + mainActivity.getAPI());
         }
 
         @Override
         protected void onPreExecute() {
                 super.onPreExecute();
-                main.getTemperature().setVisibility(View.VISIBLE);
-                main.getAddress().setVisibility(View.VISIBLE);
-                main.getLastUpdate().setVisibility(View.VISIBLE);
         }
 
         @Override
@@ -55,22 +43,23 @@ public class WeatherTask extends AsyncTask<String, Void, String> {
                         Long updatedAt = jsonObj.getLong("dt");
                         String updatedAtText = "Обновлено: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
                         String temp = main.getString("temp") + " °C";
-                        String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
-                        String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
-                        String pressure = main.getString("pressure");
-                        String humidity = main.getString("humidity");
-
-                        Long sunrise = sys.getLong("sunrise");
-                        Long sunset = sys.getLong("sunset");
-                        String windSpeed = wind.getString("speed");
-                        String weatherDescription = weather.getString("description");
-
+//                        String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+//                        String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+//                        String pressure = main.getString("pressure");
+//                        String humidity = main.getString("humidity");
+//                        Long sunrise = sys.getLong("sunrise");
+//                        Long sunset = sys.getLong("sunset");
+//                        String windSpeed = wind.getString("speed");
+//                        String weatherDescription = weather.getString("description");
                         String address = jsonObj.getString("name");
 
-                        temperature.setText(temp);
-                        addresss.setText(address);
-                        lastUpdate.setText(updatedAtText);
-                        progressBar.setVisibility(View.INVISIBLE);
+                        mainActivity.getTemperature().setText(temp);
+                        mainActivity.getAddress().setText(address);
+                        mainActivity.getLastUpdate().setText(updatedAtText);
+                        mainActivity.getProgressBar().setVisibility(View.INVISIBLE);
+                        mainActivity.getTemperature().setVisibility(View.VISIBLE);
+                        mainActivity.getAddress().setVisibility(View.VISIBLE);
+                        mainActivity.getLastUpdate().setVisibility(View.VISIBLE);
 
                 } catch (JSONException ignored) {
                         System.out.println("lol");
